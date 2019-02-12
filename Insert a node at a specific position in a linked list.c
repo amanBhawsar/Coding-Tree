583b06d8506790b63f6558608f2ba589c47876c2
Aman Bhawsar <40451824+amanBhawsar@users.noreply.github.com>
2019-02-12 22:02:09 +0530

    HackerRank

---
+++ Insert a node at a specific position in a linked list.c
@@ -0,0 +1,29 @@
+// Complete the insertNodeAtPosition function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+SinglyLinkedListNode* insertNodeAtPosition(SinglyLinkedListNode* head, int data, int position) {
+    SinglyLinkedListNode* temp=head;
+    SinglyLinkedListNode* newNode = (SinglyLinkedListNode*)malloc(sizeof(SinglyLinkedListNode));
+    newNode->data=data;
+    newNode->next=NULL;
+    if(position==0){
+        newNode->next=head;
+        head=newNode;
+        return head;
+    }
+    position=position-1;
+    while(position--){
+        temp=temp->next;
+    }
+    newNode->next=temp->next;
+    temp->next=newNode;
+    return head;
+}
---
+++ Linked Lists Detect a Cycle.c
@@ -0,0 +1,22 @@
+/*
+Detect a cycle in a linked list. Note that the head pointer may be 'NULL' if the list is empty.
+
+A Node is defined as:
+    struct Node {
+        int data;
+        struct Node* next;
+    }
+*/
+
+bool has_cycle(Node* head) {
+    Node* slow=head;
+    Node* fast=head;
+    while(fast&&fast->next){
+        slow=slow->next;
+        fast=fast->next->next;
+        if(slow==fast){
+            return true;
+        }
+    }
+    return false;
+}
