4729f9682c87fb113cc773ae91e1f3613d67a11f
Aman Bhawsar <40451824+amanBhawsar@users.noreply.github.com>
2019-04-19 18:22:07 +0530

    HackerRank

---
+++ Mark and Toys.cpp
@@ -0,0 +1,17 @@
+// Complete the maximumToys function below.
+int maximumToys(vector<int> prices, int k) {
+    int toys=0;
+    sort(prices.begin(),prices.end());
+    int sum=0,i=0;
+    while(1){
+        if(prices[i]<k){
+            toys++;
+            k=k-prices[i];
+            i++;
+        }
+        else{
+            break;
+        }
+    }
+    return toys;
+}
---
+++ Sorting Comparator.cpp
@@ -0,0 +1,20 @@
+class Checker{
+public:
+  	// complete this method
+    static int comparator(Player a, Player b)  {
+        if(a.score > b.score){
+            return 1;
+        }
+        else if(a.score < b.score){
+            return -1;
+        }
+        else{
+            if(a.name.compare(b.name)<0){
+                return 1;
+            }
+            else{
+                return -1;
+            }
+        }
+    }
+};
