94f813814a51e0cd1da9a02a2d57bc20e47b0f8b
Aman Bhawsar <40451824+amanBhawsar@users.noreply.github.com>
2019-06-11 07:49:34 +0530

    HackerEarth

---
+++ Calculate the Power.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+long long power(int n,unsigned long long k){
+    if(k==0){
+        return 1;
+    }
+    unsigned long long temp=power(n,k/2)%1000000007;
+    if(k%2==0){
+        return (temp*temp)%1000000007;
+    }else{
+        return (n*((temp*temp)%1000000007))%1000000007;
+    }
+}
+
+int main() {
+	unsigned long long a,b;
+	cin >> a >> b;									
+	cout << power(a,b)<< endl;		
+}
