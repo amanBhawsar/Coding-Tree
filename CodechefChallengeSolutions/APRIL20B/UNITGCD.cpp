081496f0d99da9fde15c5f9139bde5baf781b098
NavyaIyanampudi <68003146+NavyaIyanampudi@users.noreply.github.com>
2020-10-19 23:41:08 +0530

    Update UNITGCD.cpp

--- CodechefChallengeSolutions/APRIL20B/UNITGCD.cpp
+++ CodechefChallengeSolutions/APRIL20B/UNITGCD.cpp
@@ -1,26 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-/* set<int>v;
-int prime(int n)
-{
-    bool  prime[n+1]; 
-   
-    memset(prime, true, sizeof(prime)); 
-  
-    for (int p=2; p*p<=n; p++) 
-    { 
-        if (prime[p] == true) 
-        { 
-            for (int i=p*p; i<=n; i += p) 
-                prime[i] = false; 
-        } 
-    } 
-    v.insert(1);
-    for (int p=2; p<=n; p++) 
-       if (prime[p]) 
-           v.insert(p);
-} */
-
 int main() {
 	// your code goes here
 	long long t;
